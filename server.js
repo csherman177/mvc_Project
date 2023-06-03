7a4e222b98c3f95b634f72c20064a4914a3b921f
Courtney Sherman <csherman177@gmail.com>
2023-06-03 12:40:03 -0600

    updated the server.js file

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
+//Brings in dependenices
 const express = require("express");
-// Import and require mysql2
 const mysql = require("mysql2");
 const path = require("path");
 const hbs = exphbs.create({});
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+
+// Brings in the controller routes
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+
+// Establishes the express.js route
+const app = express();
+//Establishes the port that will be used.
+const PORT = process.env.PORT || 3001;
+
+//Sets up the view engine for the express.js using handlebars.
+app.engine("handlebars", hbs.engine);
+// Informs express.js that views should be rendered using the handlebars engine
+app.set("view engine", "handlebars");
+
+//Adds middleware to parse incoming requests with JSON payloads
+app.use(express.json());
+//Middleware to parse incoming requests with URL payloads
+app.use(express.urlencoded({ extended: true }));
+//middleware to serve static files from directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// middleware used to handle the routing between 
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
