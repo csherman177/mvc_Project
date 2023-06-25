a3d94d4014003b911c92157becb6b1d6efd57f82
Courtney Sherman <csherman177@gmail.com>
2023-06-24 18:51:21 -0600

    made a change to logout route

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,25 +1,23 @@
 function logout(event) {
-    event.preventDefault();
-  
-    fetch("/api/users/logout", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      
-      .then((response) => {
-        if (response.ok) {
-          document.location.replace('/api/login');
-        } else {
-          throw new Error("Logout failed!");
-        }
-      })
-      .catch((error) => {
-        alert("Logout failed");
-        console.log(error);
-      });
-  }
-  
+  event.preventDefault();
 
-  document.querySelector(".nav-link[href='/logout']").addEventListener("click", logout);
+  fetch("/api/users/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        document.location.replace("/api/users/login");
+      } else {
+        throw new Error("Logout failed!");
+      }
+    })
+    .catch((error) => {
+      alert("Logout failed");
+      console.log(error);
+    });
+}
+
+document.querySelector("logout").addEventListener("click", logout);
