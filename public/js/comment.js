e7ced25368692f5ad378e18bfb576ebf66e11197
Courtney Sherman <csherman177@gmail.com>
2023-07-01 10:07:05 -0600

    updated the comment route, updated dashboard to include comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,26 +1,24 @@
+function addPost(event) {
+  event.preventDefault();
+  var title = document.getElementById("Title").value.trim();
+  var content = document.getElementById("Body").value.trim();
 
-function addPost (event)  {
-    event.preventDefault()
-    var body = document.getElementById("comments").value.trim()
-    console.log(event.target.dataset.postid)
-    var post_id=event.target.dataset.postid
-    fetch (
-        "/api/comments/", {
-            method:"POST",
-            body:JSON.stringify({body, post_id}),
-            headers:{
-                "Content-Type":"application/json"
-            }
-        }
-    ).then((response)=>response.json())
-    .then((data)=>{
-        console.log(data)
-        document.location.reload()
-        
-    }) .catch(error =>{
-        console.log(error)
-        alert("Bad Request")
+  fetch("/api/posts", {
+    method: "POST",
+    body: JSON.stringify({ title, content }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.location.reload();
     })
+    .catch((error) => {
+      console.log(error);
+      alert("Bad Request");
+    });
 }
 
-document.querySelector(".com-form").addEventListener("submit", addPost)
+document.getElementById("blogSection").addEventListener("submit", addPost);
